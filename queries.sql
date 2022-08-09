ab03050b564f9b5eb826ee15a5aa59f04511e09d
ridariazz <106577074+ridariazz@users.noreply.github.com>
2022-08-08 17:13:16 -0700

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -125,3 +125,71 @@ ORDER BY de.dept_no;
 
 SELECT * FROM retire_by_dept;
 
+-- create a new table that has all the information and uses the emp_no column as the pk
+-- Salaries table a to_date column in it 
+SELECT * FROM salaries;
+
+-- sorting out the columns in descending order
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- filtering employee table and being saved into a new table emp_info
+SELECT emp_no,
+    first_name,
+last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+DROP TABLE emp_info;
+
+-- we need to join it to the salaries table to add the to_date and Salary columns 
+-- joining salaries and emp_info tables 
+SELECT e.emp_no,
+e.first_name,
+e.last_name, 
+e.gender,
+s.salary,
+de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s 
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+SELECT * FROM emp_info;
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+SELECT * FROM manager_info;
+
+-- department retirees 
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
